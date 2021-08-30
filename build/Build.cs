1ecd84716adb24d96176accc72ae773d8996cba9
Christian Sindberg <chrsin@gmail.com>
2021-08-30 23:17:50 +0200

    Added the Pack target

--- build/Build.cs
+++ build/Build.cs
@@ -1,17 +1,12 @@
-using System;
-using System.Linq;
 using Nuke.Common;
 using Nuke.Common.CI;
 using Nuke.Common.Execution;
 using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
-using Nuke.Common.Tooling;
 using Nuke.Common.Tools.DotNet;
 using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
 using static Nuke.Common.IO.FileSystemTasks;
-using static Nuke.Common.IO.PathConstruction;
 using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
@@ -28,6 +23,8 @@ class Build : NukeBuild
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
     readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+
+    [Parameter("Version of the package to produce")] readonly string Version = "0.1.0";
 
     [Solution] readonly Solution Solution;
     [GitRepository] readonly GitRepository GitRepository;
@@ -73,4 +70,22 @@ class Build : NukeBuild
             );
         });
 
+    const string NugetProjectName = "Sirups.Dapper.NodaTime";
+    Project NuGetProject => Solution.GetProject(NugetProjectName);
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .Produces(OutputDirectory + $"{NugetProjectName}.*nupgk")
+        .Executes(() =>
+        {
+            DotNetPack(_ => _
+                .SetConfiguration(Configuration)
+                .EnableNoBuild()
+                .EnableNoRestore()
+                .SetProject(NuGetProject)
+                .SetOutputDirectory(OutputDirectory)
+                .EnableIncludeSource()
+                .EnableIncludeSymbols()
+                .SetVersion(Version));
+        });
 }
