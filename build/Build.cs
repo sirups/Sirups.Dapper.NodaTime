600bad467948e483fdb3d11151cc1014315865ab
Christian Sindberg <chrsin@gmail.com>
2021-08-31 00:09:58 +0200

    Added a target to publish the NugetPackage

--- build/Build.cs
+++ build/Build.cs
@@ -11,81 +11,91 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
 [CheckBuildProjectConfigurations]
 [ShutdownDotNetAfterServerBuild]
-class Build : NukeBuild
-{
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+class Build : NukeBuild {
+	/// Support plugins are available for:
+	///   - JetBrains ReSharper        https://nuke.build/resharper
+	///   - JetBrains Rider            https://nuke.build/rider
+	///   - Microsoft VisualStudio     https://nuke.build/visualstudio
+	///   - Microsoft VSCode           https://nuke.build/vscode
+	public static int Main() => Execute<Build>(x => x.Compile);
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+	[Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
+	readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+	[Parameter("Version of the package to produce")] readonly string Version = "0.1.0";
+	[Secret] [Parameter("The ApiKey for nuget.org")] readonly string NugetApiKey = "";
 
-    [Parameter("Version of the package to produce")] readonly string Version = "0.1.0";
+	[Solution(GenerateProjects = true)] readonly Solution Solution;
+	[GitRepository] readonly GitRepository GitRepository;
 
-    [Solution] readonly Solution Solution;
-    [GitRepository] readonly GitRepository GitRepository;
+	AbsolutePath SourceDirectory => RootDirectory / "src";
+	AbsolutePath TestsDirectory => RootDirectory / "test";
+	AbsolutePath OutputDirectory => RootDirectory / "output";
 
-    AbsolutePath SourceDirectory => RootDirectory / "src";
-    AbsolutePath TestsDirectory => RootDirectory / "test";
-    AbsolutePath OutputDirectory => RootDirectory / "output";
+	Target Clean => _ => _
+		.Before(Restore)
+		.Executes(() =>
+		{
+			SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+			TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
+			EnsureCleanDirectory(OutputDirectory);
+		});
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-            SourceDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            TestsDirectory.GlobDirectories("**/bin", "**/obj").ForEach(DeleteDirectory);
-            EnsureCleanDirectory(OutputDirectory);
-        });
+	Target Restore => _ => _
+		.Executes(() =>
+		{
+			DotNetRestore(s => s
+				.SetProjectFile(Solution));
+		});
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-            DotNetRestore(s => s
-                .SetProjectFile(Solution));
-        });
+	Target Compile => _ => _
+		.DependsOn(Restore)
+		.Executes(() =>
+		{
+			DotNetBuild(s => s
+				.SetProjectFile(Solution)
+				.SetConfiguration(Configuration)
+				.EnableNoRestore());
+		});
 
-    Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-            DotNetBuild(s => s
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .EnableNoRestore());
-        });
+	Target Test => _ => _
+		.DependsOn(Compile)
+		.Executes(() =>
+		{
+			DotNetTest(s => s
+				.SetProjectFile(Solution)
+				.SetConfiguration(Configuration)
+				.EnableNoRestore()
+			);
+		});
 
-    Target Test => _ => _
-        .DependsOn(Compile)
-        .Executes(() =>
-        {
-            DotNetTest(s => s
-                .SetProjectFile(Solution)
-                .SetConfiguration(Configuration)
-                .EnableNoRestore()
-            );
-        });
+	const string NugetProjectName = "Sirups.Dapper.NodaTime";
+	AbsolutePath NugetPackageFullPath => OutputDirectory / $"{NugetProjectName}.{Version}.nupkg";
 
-    const string NugetProjectName = "Sirups.Dapper.NodaTime";
-    Project NuGetProject => Solution.GetProject(NugetProjectName);
+	Target CreateNugetPackage => _ => _
+		.DependsOn(Compile)
+		.Produces(OutputDirectory + $"{NugetProjectName}.*nupgk")
+		.Executes(() =>
+		{
+			DotNetPack(_ => _
+				.SetConfiguration(Configuration)
+				.EnableNoBuild()
+				.EnableNoRestore()
+				.SetProject(Solution.src.Sirups_Dapper_NodaTime)
+				.SetOutputDirectory(OutputDirectory)
+				.EnableIncludeSource()
+				.EnableIncludeSymbols()
+				.SetVersion(Version));
+		});
 
-    Target Pack => _ => _
-        .DependsOn(Compile)
-        .Produces(OutputDirectory + $"{NugetProjectName}.*nupgk")
-        .Executes(() =>
-        {
-            DotNetPack(_ => _
-                .SetConfiguration(Configuration)
-                .EnableNoBuild()
-                .EnableNoRestore()
-                .SetProject(NuGetProject)
-                .SetOutputDirectory(OutputDirectory)
-                .EnableIncludeSource()
-                .EnableIncludeSymbols()
-                .SetVersion(Version));
-        });
+	Target PushNugetPackage => _ => _
+		.DependsOn(CreateNugetPackage)
+		.Executes(() =>
+		{
+			DotNetNuGetPush(_ => _
+				.SetApiKey(NugetApiKey)
+				.SetTargetPath(NugetPackageFullPath)
+				.SetSource("nuget.org")
+				.EnableForceEnglishOutput());
+		});
 }
