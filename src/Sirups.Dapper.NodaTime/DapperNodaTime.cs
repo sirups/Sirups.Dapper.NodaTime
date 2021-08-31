1188c9caef87d7e8f9ba1d2e8bc68823ba6a3ecd
Christian Sindberg <chrsin@gmail.com>
2021-09-01 00:20:11 +0200

    Added a static registration for all the type handlers

---
+++ src/Sirups.Dapper.NodaTime/DapperNodaTime.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Dapper;
+using Sirups.Dapper.NodaTime.TypeHandlers;
+
+namespace Sirups.Dapper.NodaTime {
+	[ExcludeFromCodeCoverage]
+	public static class DapperNodaTime {
+		public static void RegisterAll() {
+			SqlMapper.AddTypeHandler(DurationTypeHandler.Instance);
+			SqlMapper.AddTypeHandler(InstantTypeHandler.Instance);
+			SqlMapper.AddTypeHandler(LocalDateTypeHandler.Instance);
+			SqlMapper.AddTypeHandler(LocalTimeTypeHandler.Instance);
+			SqlMapper.AddTypeHandler(OffsetTimeTypeHandler.Instance);
+			SqlMapper.AddTypeHandler(PeriodTypeHandler.Instance);
+		}
+	}
+}
